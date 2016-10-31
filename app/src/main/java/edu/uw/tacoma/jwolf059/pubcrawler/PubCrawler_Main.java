a2abb582d3e06b5d47bd21ab36fc699641942a0f
Jeremy Wolf <Jeremy Wolf>
2016-10-30 21:10:52 -0700

    Initial commit

---
+++ app/src/main/java/edu/uw/tacoma/jwolf059/pubcrawler/PubCrawler_Main.java
@@ -0,0 +1,33 @@
+package edu.uw.tacoma.jwolf059.pubcrawler;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class PubCrawler_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pub_crawler__main);
+        getActionBar().hide();
+
+
+
+    }
+
+    public void findAPub(View view) {
+        Intent intent = new Intent(this, FindpubActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void crawlPage(View view) {
+        Intent intent = new Intent(this, CrawlActivity.class);
+        startActivity(intent);
+
+    }
+}
