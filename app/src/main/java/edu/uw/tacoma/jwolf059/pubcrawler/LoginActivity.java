d1850ccda8aaad45420c87b057ab187972922b63
thangsea <thangvn@uw.edu>
2016-11-07 17:35:52 -0800

    Register UI built. Actions not yet implemented.

--- app/src/main/java/edu/uw/tacoma/jwolf059/pubcrawler/LoginActivity.java
+++ app/src/main/java/edu/uw/tacoma/jwolf059/pubcrawler/LoginActivity.java
@@ -1,28 +1,27 @@
+/*
+* CrawlActivity - PubCrawler Applicaiton
+* TCSS450 - Fall 2016
+*
+*/
 package edu.uw.tacoma.jwolf059.pubcrawler;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageInstaller;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -35,30 +34,43 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import static android.R.attr.data;
-
+/**
+ * The LoginActivity Activity will allows for the authetication of a single user. It provides each
+ * user the oppurtunity to register for a pubCrawler account, sign in using the users pubCrawler
+ * credentials, or sign in using facebook.
+ * @version 2 Nov 2016
+ * @author Jeremy Wolf
+ *
+ */
 public class LoginActivity extends AppCompatActivity {
 
     private final static String REGISTER_USER_URL = "http://cssgate.insttech.washington.edu/~jwolf059/register.php?";
-    private SharedPreferences mSharedPreferences;
     private final static String SIGNIN_URL = "http://cssgate.insttech.washington.edu/~jwolf059/authenticate.php?";
+    private SharedPreferences mSharedPreferences;
     CallbackManager callbackManager;
     private String mloginID;
     private String mPassword;
     private LoginButton loginButton;
 
-    @Override
 
+    /**
+     * Creates the Login Activity.
+     *{@inheritDoc}
+     */
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(this.getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
-
         mSharedPreferences = getSharedPreferences(getString(R.string.LOGIN_PREFS), Context.MODE_PRIVATE);
+
+        //Checks to see if the device has an active login.
         if (!mSharedPreferences.getBoolean(getString(R.string.LOGGEDIN), false)) {
             setContentView(R.layout.activity_login);
             final EditText userIdText = (EditText) findViewById(R.id.user_name);
             final EditText pwdText = (EditText) findViewById(R.id.password);
+
+            //Creats the Facebook loginbutton
             loginButton = (LoginButton) findViewById(R.id.login_button);
             loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
 
@@ -88,15 +100,18 @@ public class LoginActivity extends AppCompatActivity {
 
             });
 
-            Log.i("Past", "Facebook");
-            loginButton.clearPermissions();
+            //loginButton.clearPermissions();
 
+
+            //Creates the custom signinButton
             Button signInButton = (Button) findViewById(R.id.sign_in);
             signInButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     mloginID = userIdText.getText().toString();
                     mPassword = pwdText.getText().toString();
+
+                    //Validates that the Username Field
                     if (TextUtils.isEmpty(mloginID)) {
                         Toast.makeText(v.getContext(), "Enter your Email Address"
                                 , Toast.LENGTH_SHORT)
@@ -104,6 +119,7 @@ public class LoginActivity extends AppCompatActivity {
                         userIdText.requestFocus();
                         return;
                     }
+                    //Verifies that an email is used
                     if (!mloginID.contains("@")) {
                         Toast.makeText(v.getContext(), "Enter a valid email address"
                                 , Toast.LENGTH_SHORT)
@@ -111,7 +127,7 @@ public class LoginActivity extends AppCompatActivity {
                         userIdText.requestFocus();
                         return;
                     }
-
+                    //Validates that the password field is not empty
                     if (TextUtils.isEmpty(mPassword)) {
                         Toast.makeText(v.getContext(), "Enter password"
                                 , Toast.LENGTH_SHORT)
@@ -119,6 +135,8 @@ public class LoginActivity extends AppCompatActivity {
                         pwdText.requestFocus();
                         return;
                     }
+
+                    //Validates that the password is not less the 6 Characters.
                     if (mPassword.length() < 6) {
                         Toast.makeText(v.getContext()
                                 , "Enter password of at least 6 characters"
@@ -127,35 +145,41 @@ public class LoginActivity extends AppCompatActivity {
                         pwdText.requestFocus();
                         return;
                     }
-                    //NEED THIS
                     String url = buildSignInURL(v);
-                    Log.i("URL", url);
-                    logIn(url, v);
+                    logIn(url);
                 }
             });
         } else {
+            //Launches Main Activity if Active loggin is found.
             Intent i = new Intent(this, PubCrawler_Main.class);
             startActivity(i);
             finish();
         }
     }
 
+    /**
+     *{@inheritDoc}
+     */
     @Override
-
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
 
-    //NEED THIS
+    /**
+     * Creates the Login URL to be sent to the PHP server code that will Autheticate the users
+     * credentails
+     * @param v the Current View
+     * @return String that contains the URL to inlude login ID and Password.
+     */
     private String buildSignInURL(View v) {
 
         StringBuilder sb = new StringBuilder(SIGNIN_URL);
 
         try {
-            Log.i("Login ID ", mloginID);
+
             sb.append("id='");
-            sb.append(mloginID);
+            sb.append(mloginID.toLowerCase());
             sb.append("'");
 
 
@@ -175,26 +199,37 @@ public class LoginActivity extends AppCompatActivity {
         return sb.toString();
     }
 
-
-    public void logIn(String url, View v) {
+    /**
+     * Transfers the Loging URL to the methods that will execute the authetication.
+     * @param url String that contains the URL to inlude login ID and Password.
+     */
+    public void logIn(String url) {
         LoginTask task = new LoginTask();
         task.execute(new String[]{url.toString()});
     }
+
 
     public void register(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
 
-    //NEED this
+    /**
+     * Creates the LoginTask that executes the authetification and verifys the returned results.
+     */
     private class LoginTask extends AsyncTask<String, Void, String> {
 
-
+        /**
+         *{@inheritDoc}
+         */
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
         }
 
+        /**
+         *{@inheritDoc}
+         */
         @Override
         protected String doInBackground(String... urls) {
             String response = "";
@@ -243,6 +278,7 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "You have successfully Logged In"
                             , Toast.LENGTH_LONG)
                             .show();
+                    //Adds active login boolean to the Shared Preferences.
                     mSharedPreferences
                             .edit()
                             .putBoolean(getString(R.string.LOGGEDIN), true)
